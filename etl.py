400da38b7cdf034b4252a01db01c2aa4d66ee603
Anderson Dutra <andutra@live.com>
2021-05-23 16:08:09 -0300

    Reading data from song_data and writing parquet with song_table

--- etl.py
+++ etl.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 import configparser
 from datetime import datetime
 import os
@@ -9,8 +10,8 @@ from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dat
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
@@ -23,71 +24,80 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = f"{input_data}/song_data/*/*/*/*.json"
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
-    
+    # & df.song_id.isNotNull() & df.year.isNotNull())\
+    songs_table = df \
+        .filter(df.artist_id.isNotNull())\
+        .select("song_id", "title", "artist_id", "year", "duration")\
+        .distinct()
+
+
+    #get filepath to output song data
+    song_path = f"{output_data}/song"
+
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.parquet(song_path, mode='overwrite', partitionBy=('year', 'artist_id'))
+    #
+    # # extract columns to create artists table
+    # artists_table =
+    #
+    # # write artists table to parquet files
+    # artists_table
 
-    # extract columns to create artists table
-    artists_table = 
-    
-    # write artists table to parquet files
-    artists_table
 
-
-def process_log_data(spark, input_data, output_data):
-    # get filepath to log data file
-    log_data =
-
-    # read log data file
-    df = 
-    
-    # filter by actions for song plays
-    df = 
-
-    # extract columns for users table    
-    artists_table = 
-    
-    # write users table to parquet files
-    artists_table
-
-    # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
-    
-    # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
-    
-    # extract columns to create time table
-    time_table = 
-    
-    # write time table to parquet files partitioned by year and month
-    time_table
-
-    # read in song data to use for songplays table
-    song_df = 
-
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
-
-    # write songplays table to parquet files partitioned by year and month
-    songplays_table
+# def process_log_data(spark, input_data, output_data):
+#     # get filepath to log data file
+#     log_data =
+#
+#     # read log data file
+#     df =
+#
+#     # filter by actions for song plays
+#     df =
+#
+#     # extract columns for users table
+#     artists_table =
+#
+#     # write users table to parquet files
+#     artists_table
+#
+#     # create timestamp column from original timestamp column
+#     get_timestamp = udf()
+#     df =
+#
+#     # create datetime column from original timestamp column
+#     get_datetime = udf()
+#     df =
+#
+#     # extract columns to create time table
+#     time_table =
+#
+#     # write time table to parquet files partitioned by year and month
+#     time_table
+#
+#     # read in song data to use for songplays table
+#     song_df =
+#
+#     # extract columns from joined song and log datasets to create songplays table
+#     songplays_table =
+#
+#     # write songplays table to parquet files partitioned by year and month
+#     songplays_table
 
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
-    output_data = ""
+    # input_data = "s3a://udacity-dend/"
+    input_data = "./data/"
+    output_data = "./output_data/"
     
     process_song_data(spark, input_data, output_data)    
-    process_log_data(spark, input_data, output_data)
+    # process_log_data(spark, input_data, output_data)
 
 
 if __name__ == "__main__":
