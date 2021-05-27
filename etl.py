c92914a7cb3c216af3e9a52a1acc32fe367ee2e2
Anderson Dutra <andutra@live.com>
2021-05-27 17:50:33 -0300

    Finished, working and saving songplays

--- etl.py
+++ etl.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
 import configparser
-from datetime import datetime
 import os
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import udf, dayofweek, from_unixtime, row_number
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear
 
 clean_output_tables = False
 
@@ -83,42 +82,62 @@ def process_log_data(spark, input_data, output_data):
     users_table_path = f"{output_data}/user/"
 
     # write users table to parquet files
-    users_table.write.parquet(users_table_path, mode=save_mode)
+    users_table.write.parquet(users_table_path, mode=save_mode, partitionBy='user_id')
 
     # create timestamp column from original timestamp column
 
     get_timestamp = udf(lambda x: x // 1000)
-    time_dataframe = log_dataframe.withColumn('timestamp', get_timestamp('ts')).select('timestamp')
+    log_dataframe = log_dataframe.withColumn('timestamp', get_timestamp('ts'))
 
     # create datetime column from original timestamp column
-    get_datetime = udf(lambda x: datetime.fromtimestamp(x))
-    time_dataframe = time_dataframe.withColumn('datetime', get_datetime('timestamp'))
-    time_dataframe.show()
+    log_dataframe = log_dataframe.withColumn('start_time', from_unixtime(log_dataframe.timestamp))
+    time_dataframe = log_dataframe.select('start_time')
 
     # extract columns to create time table
     #start_time, hour, day, week, month, year, weekday
-    time_table =
-#
-#     # write time table to parquet files partitioned by year and month
-#     time_table
-#
-#     # read in song data to use for songplays table
-#     song_df =
-#
-#     # extract columns from joined song and log datasets to create songplays table
-#     songplays_table =
-#
-#     # write songplays table to parquet files partitioned by year and month
-#     songplays_table
+    time_dataframe = time_dataframe.withColumn('hour', hour(time_dataframe.start_time)) \
+        .withColumn('day', dayofmonth(time_dataframe.start_time))\
+        .withColumn('month', month(time_dataframe.start_time)) \
+        .withColumn('week', weekofyear(time_dataframe.start_time))\
+        .withColumn('year', year(time_dataframe.start_time))\
+        .withColumn('weekday', dayofweek(time_dataframe.start_time))
+
+    time_table = time_dataframe.select("start_time", "hour", "day", "week", "month", "year", "weekday")
+
+    # write time table to parquet files partitioned by year and month
+    time_table_path = f"{output_data}/time"
+    time_table.write.parquet(time_table_path, mode=save_mode, partitionBy=('start_time'))
+
+    # read in song data to use for songplays table
+    song_path = f"{output_data}/song"
+    song_df = spark.read.parquet(song_path)
+
+    # extract columns from joined song and log datasets to create songplays table
+    songplays_table = log_dataframe.join(song_df, log_dataframe.song == song_df.title)
+
+    songplays_table = songplays_table.withColumnRenamed("sessionId", "session_id")\
+        .withColumnRenamed("userAgent", "user_agent")\
+        .withColumnRenamed("userId", "user_id")
+    songplays_table = songplays_table.select('start_time', 'user_id', 'level', 'song_id', 'artist_id', 'session_id',
+                                      'location', 'user_agent')
+
+    partition_for_songplay_id = Window.orderBy('start_time', 'user_id', 'song_id', 'artist_id')
+    songplays_table = songplays_table.withColumn("songplay_id", row_number().over(partition_for_songplay_id))
+
+
+    # write songplays table to parquet files partitioned by year and month
+    songplays_path = f"{output_data}/songplay"
+    songplays_table.write.parquet(songplays_path, mode=save_mode)
 
 
 def main():
     spark = create_spark_session()
-    # input_data = "s3a://udacity-dend/"
-    input_data = "./data/"
-    output_data = "./output_data/"
+    input_data = "s3a://udacity-dend/"
+    # input_data = "./data/"
+    # output_data = "./output_data/"
+    output_data = "s3a://udacity-dend/data_warehouse"
 
-    # process_song_data(spark, input_data, output_data)
+    process_song_data(spark, input_data, output_data)
     process_log_data(spark, input_data, output_data)
 
 
